1f3d0341510215a83fad0ae4ca742b43abfe0275
giacomotolari <tolarigiacomo@gmail.com>
2022-04-21 15:11:22 +0200

    change function

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const some = 1;
 const other = 2;
 
-function add(a, b) {
-    return a + b;
+function subtraction(a, b) {
+    return a - b;
 }
 
-console.log( add(some, other) );
-console.log( add(some, other) );
+console.log( subtraction(some, other) );
+console.log( subtraction(some, other) );
